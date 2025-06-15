9d97cc10c3799f5af3abb5c501a7ac83f42e2ee2
Axel-al <axel.albtrx@gmail.com>
2025-06-15 16:56:07 +0200

    Add notebook

--- scripts/nettoyage.py
+++ scripts/nettoyage.py
@@ -9,10 +9,12 @@ def nettoyer_csv(path):
     df = pd.read_csv(path, sep=';', usecols=lambda c: c in COLONNES_UTILES, low_memory=False)
 
     # Conversion date
-    df["AAAAMMJJHH"] = pd.to_datetime(df["AAAAMMJJHH"], format="%Y%m%d%H", errors="coerce")
+    cols = ["datetime"] + [c for c in df.columns if c != "AAAAMMJJHH"]
+    df["datetime"] = pd.to_datetime(df["AAAAMMJJHH"], format="%Y%m%d%H", errors="coerce")
+    df = df[cols]
 
     # Suppression lignes avec date invalide
-    df = df.dropna(subset=["AAAAMMJJHH"])
+    df = df.dropna(subset=["datetime"]) 
 
     # Suppression lignes vides sur les données
     df = df.dropna(how="all", subset=["T", "U", "FF", "RR1", "N"])
@@ -22,8 +24,8 @@ def nettoyer_csv(path):
 # Traitement automatique de tous les fichiers
 def traiter_tous_les_csv(departements=["05", "21", "29"]):
     for dept in departements:
-        input_dir = f"./data/{dept}"
-        output_dir = f"./clean_data/{dept}"
+        input_dir = f".\\data\\{dept}"
+        output_dir = f".\\clean_data\\{dept}"
         os.makedirs(output_dir, exist_ok=True)
 
         for fichier in os.listdir(input_dir):
@@ -33,6 +35,9 @@ def traiter_tous_les_csv(departements=["05", "21", "29"]):
                 df_clean = nettoyer_csv(chemin_entree)
 
                 # Nom du fichier nettoyé
-                nom_sortie = fichier.replace(".csv", "_clean.csv").replace(".csv.gz", "_clean.csv")
+                nom_sortie = fichier.replace(".csv", "_clean.csv").replace(".gz", "")
                 chemin_sortie = os.path.join(output_dir, nom_sortie)
                 df_clean.to_csv(chemin_sortie, index=False)
+
+if __name__ == "__main__":
+    traiter_tous_les_csv()
---
+++ scripts/utils.py
@@ -0,0 +1,11 @@
+def statistiques_base(serie):
+    return {
+        "min": serie.min(),
+        "Q1": serie.quantile(0.25),
+        "médiane": serie.median(),
+        "Q3": serie.quantile(0.75),
+        "max": serie.max(),
+        "moyenne": serie.mean(),
+        "écart-type": serie.std(),
+        "écart-type relatif": (serie.std() / serie.mean() * 100) if serie.mean() != 0 else None
+    }
